f5ec1919296386f97e2aea939ccb21b4a6b2b769
jacoboblanco <56542610+jacoboblanco@users.noreply.github.com>
2020-04-04 20:56:16 +0200

    Add files via upload

---
+++ doc/html/menudata.js
@@ -0,0 +1,21 @@
+var menudata={children:[
+{text:"Página principal",url:"index.html"},
+{text:"Namespaces",url:"namespaces.html",children:[
+{text:"Lista de 'namespaces'",url:"namespaces.html"}]},
+{text:"Archivos",url:"files.html",children:[
+{text:"Lista de archivos",url:"files.html"},
+{text:"Miembros de los ficheros",url:"globals.html",children:[
+{text:"Todo",url:"globals.html",children:[
+{text:"c",url:"globals.html#index_c"},
+{text:"d",url:"globals.html#index_d"},
+{text:"f",url:"globals.html#index_f"},
+{text:"g",url:"globals.html#index_g"},
+{text:"i",url:"globals.html#index_i"},
+{text:"m",url:"globals.html#index_m"},
+{text:"o",url:"globals.html#index_o"},
+{text:"p",url:"globals.html#index_p"},
+{text:"s",url:"globals.html#index_s"},
+{text:"v",url:"globals.html#index_v"}]},
+{text:"Funciones",url:"globals_func.html"},
+{text:"Variables",url:"globals_vars.html"},
+{text:"defines",url:"globals_defs.html"}]}]}]}
